5c2fd960a589ea1150628c5ffc8af0f61148aa77
Jurgen Alebregtse <j.alebregtse@gmail.com>
2022-12-09 22:16:36 +0100

    day 6

--- src/day_6/Supply_Stacks.py
+++ src/day_6/Supply_Stacks.py
@@ -9,7 +9,7 @@ def main():
                 and lines_list[0][char + 1] != lines_list[0][char + 2] \
                 and lines_list[0][char + 1] != lines_list[0][char + 3] \
                 and lines_list[0][char + 2] != lines_list[0][char + 3]:
-            print(char + 3)
+            print(char + 4)
             break
 
 if __name__ == "__main__":
