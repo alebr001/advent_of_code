e89a7c8be3faa62f0067484609c8bc7775bb5c00
Jurgen Alebregtse <j.alebregtse@gmail.com>
2022-12-09 23:58:45 +0100

    day 6 part b fix major bug :-p

--- src/day_6/Supply_Stacks.py
+++ src/day_6/Supply_Stacks.py
@@ -1,29 +1,35 @@
 def main():
     with open("input.txt", "r") as file:
-        lines_list = file.readlines()
+        fileline = file.readlines()
 
     # # PART A
-    for char in range(0,len(lines_list[0])-1,1):
-        if lines_list[0][char] != lines_list[0][char + 1] \
-                and lines_list[0][char] != lines_list[0][char + 2] \
-                and lines_list[0][char] != lines_list[0][char + 3] \
-                and lines_list[0][char + 1] != lines_list[0][char + 2] \
-                and lines_list[0][char + 1] != lines_list[0][char + 3] \
-                and lines_list[0][char + 2] != lines_list[0][char + 3]:
+    for char in range(0,len(fileline[0])-1,1):
+        if fileline[0][char] != fileline[0][char + 1] \
+                and fileline[0][char] != fileline[0][char + 2] \
+                and fileline[0][char] != fileline[0][char + 3] \
+                and fileline[0][char + 1] != fileline[0][char + 2] \
+                and fileline[0][char + 1] != fileline[0][char + 3] \
+                and fileline[0][char + 2] != fileline[0][char + 3]:
             print(char + 4)
             break
 
     # PART B (kan ook voor part a gebruikt worden)
-    list_of_chars = []
+    list_different_chars = []
     length_of_marker = 14
-    for char in range(len(lines_list[0])-1):
-        if lines_list[0][char] not in list_of_chars:
-            list_of_chars.append(lines_list[0][char])
-            if len(list_of_chars) == length_of_marker:
-                print(char)
+    for char in range(len(fileline[0])-1):
+        # put every new char in a list if it is not already in that list
+        if fileline[0][char] not in list_different_chars:
+            list_different_chars.append(fileline[0][char])
+            if len(list_different_chars) == length_of_marker:
+                print("length", len(list_different_chars))
+                # +1 to convert from index to i-th number
+                print(char+1)
                 break
+        # if it is already in the list, find the occurrence and delete everything preceding including the letter itself
         else:
-            list_of_chars = [lines_list[0][char]]
+            index = list_different_chars.index(fileline[0][char])
+            list_different_chars = list_different_chars[index+1:]
+            list_different_chars.append(fileline[0][char])
 
 
 if __name__ == "__main__":
